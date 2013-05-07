b9cf49f98118d5480f2ce5dc4091ae362ee9e4e8
Mark Wagner <mwagner72@gmail.com>
2013-05-07 09:36:40 -0500

    Trying to use Base Adapter

--- taskit/src/taskit_activity.rb
+++ taskit/src/taskit_activity.rb
@@ -12,12 +12,15 @@ java_import "android.widget.BaseAdapter"
 java_import "java.util.ArrayList"
 
 class TaskitActivity
-
+    TAG = "TaskitActivity"
 
   def onCreate(bundle)
     super
-    Log.e "TaskitActivity", "in onCreate"
-
+    Log.d(TAG, "****************************************************************************")
+    Log.d(TAG, "****************************************************************************")
+    Log.d "TaskitActivity", "in onCreate"
+    Log.d(TAG, "****************************************************************************")
+    Log.d(TAG, "****************************************************************************")
     set_title 'Ruboto - Task It'
 
     task = Task.new("Hello World")
@@ -28,17 +31,21 @@ class TaskitActivity
     #adapter.add("Test")
 
     @adapter = TaskItAdapter.new(self, @tasks)
-
+    Log.d(TAG, "****************************************************************************")
+    Log.d(TAG, "****************************************************************************")
     Log.e "TaskitActivity", "created adapter"
-
+    Log.d(TAG, "****************************************************************************")
+    Log.d(TAG, "****************************************************************************")
     self.setContentView(Ruboto::R::layout::task_list)
     new_task_button = findViewById(Ruboto::R::id::new_task_button)
     @task_list = findViewById(Ruboto::R::id::task_list)
     @task_list.set_adapter(@adapter)
     new_task_button.on_click_listener = proc { |view| new_task_activity }
-
+    Log.d(TAG, "****************************************************************************")
+    Log.d(TAG, "****************************************************************************")
     Log.e "TaskitActivity", "finished onCreate"
-
+    Log.d(TAG, "****************************************************************************")
+    Log.d(TAG, "****************************************************************************")
   rescue
     puts "Exception creating activity: #{$!}"
     puts $!.backtrace.join("\n")
@@ -54,36 +61,57 @@ class TaskitActivity
   end
 
   class TaskItAdapter < android.widget.BaseAdapter
-
+      TAG2 = "TaskItAdapter"
     def initialize(context, data)
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "                 Initialize       ")
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "****************************************************************************")
       @context = context
       @data = data
     end
 
-
     def getCount
-      return @data.size
-    end
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "                 Get Count          ")
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "****************************************************************************")
 
+      @data.size
+    end
 
     def getItem(position)
-      return @data.get(position)
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "                 GET ITEM       ")
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "****************************************************************************")
+      @data.get(position)
     end
-
 
     def getItemId(position)
-      return position
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "                 GET ITEMID           ")
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "****************************************************************************")
+      position
     end
-
 
     def hasStableIds
       return true
     end
 
     def getView(position, convertView, parent)
-      Log.d(TAG, "***************************************** getView ***********************************" + convertView)
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "                 GET VIEW           ")
+      Log.d(TAG2, "****************************************************************************")
+      Log.d(TAG2, "****************************************************************************")
       if convertView == null
-        convertView = LayoutInflater.from(context).inflate(Ruboto::R::id::task_list_item, null)
+        convertView = LayoutInflater.from(@context).inflate(Ruboto::R::id::task_list_item, null)
         tag = ListItemViewTag.new
         tag.title = convertView.findViewById(R.id.task)
         tag.name = convertView.findViewById(R.id.assignee)
