6ec8cbf2456acc085a1cedb13e2ab7d5919b5db2
Mark Wagner <mwagner72@gmail.com>
2013-05-10 09:21:43 -0500

    a bit more ruby like

--- taskit/src/taskit_activity.rb
+++ taskit/src/taskit_activity.rb
@@ -6,40 +6,29 @@ require 'user'
 
 ruboto_import_widgets :Button, :LinearLayout, :RelativeLayout, :TextView, :ListView
 
-java_import "android.util.Log"
-java_import "android.widget.ArrayAdapter"
-java_import "android.widget.BaseAdapter"
-java_import "java.util.ArrayList"
-java_import "android.view.LayoutInflater"
-java_import "android.os.AsyncTask"
-java_import "org.apache.http.client.HttpClient"
-java_import "org.apache.http.impl.client.DefaultHttpClient"
-java_import "org.apache.http.client.methods.HttpGet"
-java_import "java.io.BufferedReader"
-java_import "java.lang.StringBuilder"
-java_import "java.io.InputStreamReader"
-java_import "org.json.JSONArray"
+java_import 'android.util.Log'
+java_import 'android.widget.ArrayAdapter'
+java_import 'android.widget.BaseAdapter'
+java_import 'java.util.ArrayList'
+java_import 'android.view.LayoutInflater'
+java_import 'android.os.AsyncTask'
+java_import 'org.apache.http.client.HttpClient'
+java_import 'org.apache.http.impl.client.DefaultHttpClient'
+java_import 'org.apache.http.client.methods.HttpGet'
+java_import 'java.io.BufferedReader'
+java_import 'java.lang.StringBuilder'
+java_import 'java.io.InputStreamReader'
+java_import 'org.json.JSONArray'
 
 class TaskitActivity
-  TAG = "TaskitActivity"
+  TAG = 'TaskitActivity'
   attr_accessor :tasks, :users
 
-  #def self.tasks
-  #  @tasks ||= ArrayList.new
-  #end
-  #
-  #def self.users
-  #  @users ||= ArrayList.new
-  #end
-  #
-  #def self.adapter
-  #  @adapter ||= TaskItAdapter.new(self, self.tasks)
-  #end
 
-  def onCreate(bundle)
+  def on_create(bundle)
     super
 
-    Log.d "TaskitActivity", "in onCreate"
+    Log.d TAG, "in onCreate"
 
     @users = ArrayList.new
     @tasks = ArrayList.new
@@ -47,7 +36,7 @@ class TaskitActivity
     set_title 'Ruboto - Task It'
     @adapter = TaskItAdapter.new(self, @tasks)
 
-    Log.e "TaskitActivity", "created adapter"
+    Log.e TAG, "created adapter"
 
     self.setContentView(Ruboto::R::layout::task_list)
     new_task_button = findViewById(Ruboto::R::id::new_task_button)
@@ -55,14 +44,14 @@ class TaskitActivity
     @task_list.set_adapter(@adapter)
     new_task_button.on_click_listener = proc { |view| new_task_activity }
 
-    Log.e "TaskitActivity", "finished onCreate"
+    Log.e TAG, "finished onCreate"
 
   rescue
     puts "Exception creating activity: #{$!}"
     puts $!.backtrace.join("\n")
   end
 
-  def onResume
+  def on_resume
     super
     task_fetcher = TaskFetcher.new(@users, @tasks, @adapter)
     task_fetcher.execute
@@ -115,15 +104,15 @@ class TaskitActivity
       if convertView == nil
         convertView = LayoutInflater.from(@context).inflate(Ruboto::R::layout::task_list_item, nil)
         tag         = ListItemViewTag.new
-        tag.title   = convertView.findViewById(Ruboto::R::id::task)
-        tag.name    = convertView.findViewById(Ruboto::R::id::assignee)
-        convertView.setTag(tag)
+        tag.title   = convertView.find_view_by_id(Ruboto::R::id::task)
+        tag.name    = convertView.find_view_by_id(Ruboto::R::id::assignee)
+        convertView.set_tag(tag)
       end
 
-      task = getItem(position)
-      tag  = convertView.getTag()
+      task = get_item(position)
+      tag  = convertView.get_tag
 
-      tag.title.setText(task.description)
+      tag.title.set_text(task.description)
 
       convertView
     end
@@ -139,7 +128,6 @@ class TaskitActivity
       @tasks = tasks
       @adapter = adapter
       Log.d(TAG3, "TaskFetcher Initialize")
-      #@context = context
     end
 
     def onPreExecute(param)
@@ -149,58 +137,58 @@ class TaskitActivity
     def doInBackground(*param)
 
       Log.d(TAG3, "TaskFetcher doInBackground")
-      users = getJSONfromURL('http://192.168.252.129:3000/users.json')
+      users = json_from_url('http://192.168.252.129:3000/users.json')
       for i in 0..(users.length() - 1)
-        user = users.getJSONObject(i)
-        Log.d(TAG3, user.toString)
-        @users.add User.new(user.getString("name"), user.getString("email_address"))
+        user = users.get_json_object(i)
+        Log.d(TAG3, user.to_string)
+        @users.add User.new(user.get_string("name"), user.get_string("email_address"))
       end
-      tasks = getJSONfromURL('http://192.168.252.129:3000/tasks.json')
+      tasks = json_from_url('http://192.168.252.129:3000/tasks.json')
       for i in 0..(tasks.length() - 1)
-        task = tasks.getJSONObject(i)
-        Log.d(TAG3, task.toString)
-        @tasks.add Task.new(task.getString("details"))
+        task = tasks.get_json_object(i)
+        Log.d(TAG3, task.to_string)
+        @tasks.add Task.new(task.get_string("details"))
       end
     end
 
     def onPostExecute(param)
       Log.d(TAG3, "TaskFetcher onPostExecute")
-      @adapter.notifyDataSetChanged
+      @adapter.notify_data_set_changed
     end
 
-    def getJSONfromURL(url)
+    def get_json_from_url(url)
 
 
-      #begin
+      begin
         httpclient = DefaultHttpClient.new()
         httpget   = HttpGet.new(url)
         response   = httpclient.execute(httpget)
-        entity     = response.getEntity()
-        is         = entity.getContent()
+        entity     = response.entity
+        is         = entity.content
 
-      #rescue
-      #  Log.e("log_tag", "Error in http connection ")
-      #end
+      rescue
+        Log.e("log_tag", "Error in http connection ")
+      end
 
-      #begin
+      begin
         reader = BufferedReader.new(InputStreamReader.new(is, "iso-8859-1"), 8)
         sb     = StringBuilder.new()
         line   = nil
-        while ((line = reader.readLine()) != nil)
+        while ((line = reader.read_line) != nil)
           sb.append(line + "\n")
         end
-        is.close()
-        result=sb.toString()
-      #rescue
-      #  Log.e("log_tag", "Error converting result ")
-      #end
+        is.close
+        result=sb.to_string
+      rescue
+        Log.e("log_tag", "Error converting result ")
+      end
 
       #try parse the string to a JSON object
-      #begin
+      begin
         jArray = JSONArray.new(result)
-      #rescue
-      #  Log.e("log_tag", "Error parsing data ")
-      #end
+      rescue
+        Log.e("log_tag", "Error parsing data ")
+      end
 
       jArray
 
